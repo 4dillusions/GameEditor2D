e71e28a7fd1dbae6919ba1353cf36b076d2bfbd2
Robert Oze <support@4dillusions.com>
2023-10-03 00:37:08 +0200

    [App] impl MVVM framework

---
+++ Source/GE2D.App/MVVMFramework/NotificationObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+namespace GE2D.App.MVVMFramework;
+
+public abstract class NotificationObject : INotifyPropertyChanging, INotifyPropertyChanged
+{
+    public event PropertyChangingEventHandler? PropertyChanging;
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void SetProperty<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (!EqualityComparer<T>.Default.Equals(field, value))
+        {
+            PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Source/GE2D.App/MVVMFramework/RelayCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace GE2D.App.MVVMFramework;
+
+public class RelayCommand : ICommand
+{
+    private readonly Action<object> execute;
+    private readonly Func<object, bool>? canExecute;
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add { CommandManager.RequerySuggested += value; }
+        remove { CommandManager.RequerySuggested -= value; }
+    }
+
+    public RelayCommand(Action<object> execute, Func<object, bool>? canExecute = null)
+    {
+        this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        this.canExecute = canExecute;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        if (parameter is null)
+            throw new ArgumentNullException(nameof(parameter));
+
+        return canExecute?.Invoke(parameter) ?? true;
+    }
+
+    public void Execute(object? parameter)
+    {
+        if (execute != null && parameter != null)
+            execute(parameter);
+    }
+}
